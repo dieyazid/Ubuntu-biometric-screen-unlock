327dd1874cc9e49f97aef36d62f9837143928d42
yazid <moh1mmed.ya21d@gmail.com>
2022-12-27 00:12:40 +0100

    Fix: forgot to add it

--- Desktop/main.py
+++ Desktop/main.py
@@ -1,8 +1,18 @@
+import socket
 import os
-import time
 
+IP_ADDRESS = '192.168.1.7'
+PORT = 8000
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the IP address and port
+sock.bind((IP_ADDRESS, PORT))
 os.popen('gnome-screensaver-command -l')
 while True:
-    time.sleep(5)
+    # Receive message from Flutter app
+    data, addr = sock.recvfrom(1024)
+    message = data.decode()
+    print(f'Received message from Flutter: {message}')
     os.popen('gnome-screensaver-command -d')
-    break
